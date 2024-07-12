c818cde5fa0116e0e8f88c8bfc608ba1f3eb56d5
Sérgio Filho <sergiofilhoja@hotmail.com>
2024-07-12 18:12:29 -0300

    statistics

--- components/home/card-balance1.tsx
+++ components/home/card-balance1.tsx
@@ -1,9 +1,9 @@
 import { Card, CardBody } from "@nextui-org/react";
-import { useReclamacoes } from "../hooks/useReclamacoes";
+import { useFormSubmissions } from "../hooks/useSubmitsWp";
 import { Community } from "../icons/community";
 
 export const CardBalance1 = () => {
-  const { data: reclamacoes } = useReclamacoes();
+  const { data } = useFormSubmissions();
 
   return (
     <Card className="xl:max-w-sm bg-primary rounded-xl shadow-md px-3 w-full">
@@ -16,7 +16,7 @@ export const CardBalance1 = () => {
         </div>
         <div className="flex gap-2.5 py-2 items-center">
           <span className="text-white text-xl font-semibold">
-            {reclamacoes?.length} reclamações
+            {data?.data?.length} reclamações
           </span>
         </div>
       </CardBody>
